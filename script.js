9b80d527e1e14bf4ab6f788dace68e7c5cc6b811
Talika Bajaj <bajajtalika2001@gmail.com>
2024-02-12 20:28:24 +0530

    Add function to enable submit button

--- script.js
+++ script.js
@@ -1,50 +1,59 @@
 let uniData = document.querySelector(".uni-data");
 let uniName = document.getElementById("uni-name");
-let  uniCountry= document.getElementById("uni-country");
+let uniCountry = document.getElementById("uni-country");
 
 
 //use fetch 
-let url = "http://universities.hipolabs.com/search?";
 
 // async function getUniversity () {
-async function getUniversity (name,country) {
-    let response = await fetch(url + "name=" + name + "&country=" +  country);
-    // let response = await fetch(url + name + country);
-    let data  = await response.json();
-    console.log(data);
-    // let universityName = data[0].name;
-    
-    data.forEach(element => {
-        uniData.innerHTML += `<div class="card" style="width: 18rem; padding: 15px; cursor:pointer" onmouseover = "scaleUp(this)" onmouseout = "scaleDown(this)">
-        <div class="card-body">
-        <h5 class="card-title">${element.name}</h5><br>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Country: ${element.country}</h6>
-        <a href="${element.web_pages}" target="_blank" class="btn btn-primary">Vist Website</a>
-        </div>
-        </div>`
-    });
+async function getUniversity(name, country) {
 
-    
+    let response = await fetch("http://universities.hipolabs.com/search?name=" + name + "&country=" + country);
+    let data = await response.json();
+    console.log(data);
+
+    if (data.length > 0) {
+
+        data.forEach(element => {
+            uniData.innerHTML += `<div class="card" style="width: 18rem; padding: 15px; cursor:pointer" onmouseover = "scaleUp(this)" onmouseout = "scaleDown(this)">
+                <div class="card-body">
+                <h5 class="card-title">${element.name}</h5><br>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Country: ${element.country}</h6>
+                <a href="${element.web_pages}" target="_blank" class="btn btn-primary">Vist Website</a>
+                </div>
+                </div>`
+        });
+
+    } else {
+        uniData.innerHTML = `<p style= "font-size: 30px">Error: University not found</p>`;
+    }
+
     document.getElementById("uni-name").value = "";
     document.getElementById("uni-country").value = "";
 }
 
-getUniversity()
+getUniversity();
 
-document.getElementById("search-uni").addEventListener("submit", (e)=> {
+
+document.getElementById("search-uni").addEventListener("submit", (e) => {
     e.preventDefault();
     uniData.style.display = "flex";
     // uniData.innerHTML = "fkjvnjvnjnjnvjvnivner";
     getUniversity(uniName.value, uniCountry.value)
     uniData.innerHTML = "";
+    // uniData.style.display = "none";
 })
 
-function scaleUp (x) {
+function scaleUp(x) {
     x.style.transform = "scale(1.1)";
     x.style.transition = "0.5s ease";
 }
 
-function scaleDown (x) {
+function scaleDown(x) {
     x.style.transform = "scale(1)";
     x.style.transition = "0.5s ease";
+}
+
+function enableButton () {
+    document.getElementById("search").disabled = false;
 }
