2bfa0d09cbb177bce1bb4b424f55a3c8f2df21fd
Talika-Bajaj <154504007+Talika-Bajaj@users.noreply.github.com>
2024-03-15 15:40:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ let uniCountry = document.getElementById("uni-country");
 
 // function to fetch api
 async function getUniversity(name, country) {
-    let response = await fetch("https://universities.hipolabs.com/search?name=" + name + "&country=" + country);
+    let response = await fetch("http://universities.hipolabs.com/search?name=" + name + "&country=" + country,   method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
     let data = await response.json();
     console.log(data);
 
